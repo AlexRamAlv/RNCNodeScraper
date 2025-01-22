547485ea3c8835b09c6d6b6d0e27c2b561a422bd
Alexander Ramírez Álvarez <69972658+AlexRamAlv@users.noreply.github.com>
2025-01-21 20:49:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import puppeteer from "puppeteer";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello from express" });
+});
+
+app.post("/scrape", async (req, res) => {
+  const url =
+    "https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/rnc.aspx";
+  const { rnc } = req.body;
+
+  if (!rnc) {
+    return res.status(400).json({ Error: "RNC is required" });
+  }
+
+  try {
+    const browser = await puppeteer.launch(/*{ headless: true }*/);
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.type("#ctl00_cphMain_txtRNCCedula", rnc);
+    await page.click("#ctl00_cphMain_btnBuscarPorRNC");
+    await page.waitForSelector("tbody");
+    const data = await page.evaluate(() => {
+      const rows = document.querySelectorAll("tr");
+      const result = [];
+      rows.forEach((row) => {
+        const cells = row.querySelectorAll("td");
+        const rowData = [];
+        debugger;
+        cells.forEach((cell) => {
+          rowData.push(cell.textContent);
+        });
+        result.push(rowData);
+        result;
+        resultJSON = {};
+        result.forEach((par) => {
+          const clave = par[0];
+          const valor = par[1];
+          resultJSON[clave] = valor;
+        });
+      });
+      return resultJSON;
+    });
+
+    await browser.close();
+    res.json({
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const port = parseInt(process.env.PORT) || 9002;
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}/`);
+});
